306bb726a20e6bd028eb861b28ff9422097f1c31
Pavel Klos <pavelkloscz@gmail.com>
2025-03-11 19:23:35 +0100

    30 Days of Streamlit (Day 21) - UPDATE

--- Streamlit/streamlit_app.py
+++ Streamlit/streamlit_app.py
@@ -275,13 +275,18 @@ with st.expander('About this app'):
 progress_text = "Operation in progress. Please wait."
 my_bar = st.progress(0, text=progress_text)
 
+
+
 for percent_complete in range(100):
      # time.sleep(0.05)
-     time.sleep(0.1)
-     my_bar.progress(percent_complete + 1)
+     time.sleep(0.01)
+     # my_bar.progress(percent_complete + 1)
+     my_bar.progress(percent_complete + 1, text=progress_text)
+time.sleep(1)
+my_bar.empty()
+st.balloons()
 
-if my_bar.progress == 100:
-     st.balloons()
+st.button("Rerun")
 
 # Day 22 -----------------------------------------------------------------------
 # []
